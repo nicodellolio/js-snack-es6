7ace99ac99031d9a63ee9345216032ce17364a1a
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-03-05 15:45:23 +0100

    add snack 3

---
+++ snack 3/main.js
@@ -0,0 +1,41 @@
+const animals = [
+    {
+        nome: "Rana",
+        famiglia: "Ranidae",
+        classe: "Anfibi"
+    },
+    {
+        nome: "Piccione",
+        famiglia: "Columbidae",
+        classe: "Volatili"
+    },
+    {
+        nome: "Passero",
+        famiglia: "Passeridae",
+        classe: "Volatili"
+    },
+    {
+        nome: "Gatto",
+        famiglia: "Felidae",
+        classe: "Mammiferi"
+    },
+    {
+        nome: "Papera",
+        famiglia: "Anatidi",
+        classe: "Volatili"
+    },
+    {
+        nome: "Canguro",
+        famiglia: "Macropodidae",
+        classe: "Mammiferi"
+    },
+    {
+        nome: "Megattera",
+        famiglia: "Balaenopteridae",
+        classe: "Mammiferi"
+    }
+]
+console.log(animals);
+
+const mammaliaList = animals.filter((animal) => animal.classe === "Mammiferi")
+console.log(mammaliaList);
